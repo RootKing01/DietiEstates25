d51b6b6b20083108f8370f4b5a707ba8cd52e90b
Vincenzo Noviello <you@example.com>
2025-07-16 22:09:54 +0200

    View from amplify test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { View } from '@aws-amplify/ui-react';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <View   as="div"   ariaLabel="View example"   backgroundColor="var(--amplify-colors-white)"   borderRadius="6px"   boxShadow="3px 3px 5px 6px var(--amplify-colors-neutral-60)"   color="var(--amplify-colors-blue-60)"   height="4rem"   maxWidth="100%"   padding="1rem"   width="20rem"   onClick={() => alert('🏔 What a beautiful <View>! 🔭')}   >   {"I'm a <div>! 🤩"} </View>
   );
 }
 
